7ec9b14798960107b029585a1573bf86318e352f
문찬웅 <mooncw@hankookilbo.com>
2023-10-13 10:56:46 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+process.env.NODE_ENV === "dev"
+  ? require("dotenv").config({ path: "./env/dev.env" })
+  : require("dotenv").config({ path: "./env/prod.env" });
+
+const express = require("express");
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+
+const app = express();
+const PORT = process.env.PORT || 6002;
+
+
+app.use(
+  cors({
+    origin: ["http://localhost:3001", process.env.CLIENT_URL],
+    credentials: true,
+    methods: ["HEAD", "POST", "PUT", "GET", "PATCH", "DELETE"],
+  })
+);
+
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+app.use(cookieParser());
+
+const apiProxy = createProxyMiddleware({
+  target: 'https://ulltimate-moonblog-node-production.up.railway.app',
+  changeOrigin: true,
+});
+app.use('/', apiProxy);
+
+// app.get("/", (req, res) => {
+//   console.log("!!!!!!!!")
+// })
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log(`Unable to run Server on ${PORT}=> ${err}`);
+  } else {
+    console.log(`Server Up: ${PORT}`);
+  }
+});
